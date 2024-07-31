b0fe7c0f08163bbd6f866be685ea13fedd222769
RomiGilat <117747207+RomiGilat@users.noreply.github.com>
2024-07-31 15:44:06 -0700

    Update back&forth.v

--- back&forth.v
+++ back&forth.v
@@ -1,42 +1,4 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/20/2024 01:56:52 PM
-// Design Name: 
-// Module Name: QPD
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module QPD # //Quarter Period Delay
-  (
-    parameter sample_frequency = 100000 //in Hz
-)
-  (
-    input rt,
-    input sclock,
-    input [7:0] count_quater_period, //This has the delay that the user wants built in. Check C server
-    output [0:0] trigger
-);
-    reg [31:0] counter=0;
-    reg [0:0] wantTrigger=0;
-    reg [0:0] signal=0;
-    reg [0:0] sentTrigger=0;
-    reg [7:0] new_count_quater_period=0; //This variable helps to check when new measurement parameters have been sent. If we are still using the last ones, we have not yet requested a new trigger
-    assign trigger = signal;
-   always @(posedge sclock) begin
+    always @(posedge sclock) begin
         if (rt == 1) begin
             // If a new quarter period count is received, enable the trigger request
             if (stored_count_quater_period != count_quater_period) begin
@@ -54,7 +16,8 @@ module QPD # //Quarter Period Delay
             signal <= 0;
         end
 
-        if (counter == count_quater_period) begin
+        // Add a clause to prevent triggering if counter is below 23000
+        if (counter >= 23000 && counter == count_quater_period) begin
             signal <= 1;
             sentTrigger <= 1;
             counter <= 0;
@@ -65,7 +28,7 @@ module QPD # //Quarter Period Delay
         end
         
         trigger <= signal;
-    end
+    end
 endmodule
 
 intcommand: 12
