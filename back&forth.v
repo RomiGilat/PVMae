f9cbcf19961979218780d27d75daea760575d837
RomiGilat <117747207+RomiGilat@users.noreply.github.com>
2024-07-31 15:36:19 -0700

    Update back&forth.v

--- back&forth.v
+++ back&forth.v
@@ -1,21 +1,42 @@
-module QPD // Quarter Period Delay
-#(
-    parameter sample_frequency = 100000 // in Hz
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/20/2024 01:56:52 PM
+// Design Name: 
+// Module Name: QPD
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module QPD # //Quarter Period Delay
+  (
+    parameter sample_frequency = 100000 //in Hz
 )
-(
+  (
     input rt,
     input sclock,
-    input [7:0] count_quater_period, // This has the delay that the user wants built in.
-    output reg trigger // Changed to reg to allow assignment in always block
+    input [7:0] count_quater_period, //This has the delay that the user wants built in. Check C server
+    output [0:0] trigger
 );
-
-    reg [31:0] counter = 0;
-    reg wantTrigger = 0;
-    reg signal = 0;
-    reg sentTrigger = 0;
-    reg [7:0] stored_count_quater_period = 0; // Store the current count_quater_period
-
-    always @(posedge sclock) begin
+    reg [31:0] counter=0;
+    reg [0:0] wantTrigger=0;
+    reg [0:0] signal=0;
+    reg [0:0] sentTrigger=0;
+    reg [7:0] new_count_quater_period=0; //This variable helps to check when new measurement parameters have been sent. If we are still using the last ones, we have not yet requested a new trigger
+    assign trigger = signal;
+   always @(posedge sclock) begin
         if (rt == 1) begin
             // If a new quarter period count is received, enable the trigger request
             if (stored_count_quater_period != count_quater_period) begin
@@ -46,8 +67,6 @@ module QPD // Quarter Period Delay
         trigger <= signal;
     end
 endmodule
-
-
 
 intcommand: 12
 delay: 100
