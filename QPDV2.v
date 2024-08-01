02abbd710e55418107041366be4ae5cdaf27c16a
RomiGilat <117747207+RomiGilat@users.noreply.github.com>
2024-08-01 09:49:00 -0700

    Update QPDV2.v

--- QPDV2.v
+++ QPDV2.v
@@ -1,5 +1,23 @@
-// this is for the QPD, currntly the one on file isnt working properly and killing the code 
-
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/20/2024 01:56:52 PM
+// Design Name: 
+// Module Name: QPD
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 
 module QPD # //Quarter Period Delay
@@ -18,32 +36,38 @@ module QPD # //Quarter Period Delay
     reg [0:0] sentTrigger=0;
     reg [7:0] new_count_quater_period=0; //This variable helps to check when new measurement parameters have been sent. If we are still using the last ones, we have not yet requested a new trigger
     assign trigger = signal;
-    always@(*) begin
-        if ((rt==1) && (new_count_quater_period != count_quater_period)) begin
-            wantTrigger<=1'b1;
-            
+   always @(posedge sclock) begin
+        if (rt == 1) begin
+            // If a new quarter period count is received, enable the trigger request
+            if (stored_count_quater_period != count_quater_period) begin
+                wantTrigger <= 1;
+                stored_count_quater_period <= count_quater_period;
+            end
         end
-        else if (sentTrigger==1)begin
-            wantTrigger<=1'b0;
+        
+        if(counter <= 23000) begin
+            //another check to make sure count is actually correct 
+            signal <= 0;
+            sentTrigger <= 0;
+            counter <= 0;
         end
-    end
-    always@(posedge sclock) begin
-        if (wantTrigger==1) begin
-            counter<=counter+1;
-            sentTrigger<=1'b0;
-        end
-        else begin
-            counter<=0;
-            signal<=0;
-        end
-        if (counter>=count_quater_period) begin
-            signal<=1'b1;
-            sentTrigger<=1;
-            counter<=0;
-            new_count_quater_period=count_quater_period;
+
+        if (wantTrigger == 1) begin
+            counter <= counter + 1;
+            sentTrigger <= 0;
         end
         else begin
-            signal<=0;
+            counter <= 0;
+            signal <= 0;
         end
+ // Check if the counter is above 23000 before allowing a trigger
+        if (counter == count_quater_period) begin
+            signal <= 1;
+            sentTrigger <= 1;
+            counter <= 0; // Reset counter after a trigger
+            wantTrigger <= 0; // Reset the wantTrigger after a trigger
+        end
+        
+        trigger <= signal;
     end
 endmodule
