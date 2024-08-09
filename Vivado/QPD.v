0b44f9a6b7f0939b1eb66f0664d892aae270605c
joshuamarmor <150301812+joshuamarmor@users.noreply.github.com>
2024-08-09 10:36:14 -0700

    Update QPD.v

--- Vivado/QPD.v
+++ Vivado/QPD.v
@@ -27,14 +27,14 @@ module QPD # //Quarter Period Delay
   (
     input rt,
     input sclock,
-    input [15:0] count_quater_period, //This has the delay that the user wants built in. Check C server
+    input [7:0] count_quater_period, //This has the delay that the user wants built in. Check C server
     output [0:0] trigger
 );
     reg [31:0] counter=0;
     reg [0:0] wantTrigger=0;
     reg [0:0] signal=0;
     reg [0:0] sentTrigger=0;
-    reg [15:0] new_count_quater_period=0; //This variable helps to check when new measurement parameters have been sent. If we are still using the last ones, we have not yet requested a new trigger
+    reg [7:0] new_count_quater_period=0; //This variable helps to check when new measurement parameters have been sent. If we are still using the last ones, we have not yet requested a new trigger
     assign trigger = signal;
     always@(*) begin
         if ((rt==1)&&(new_count_quater_period!=count_quater_period)) begin
@@ -58,10 +58,11 @@ module QPD # //Quarter Period Delay
             signal<=1'b1;
             sentTrigger<=1;
             counter<=0;
+            new_count_quater_period=count_quater_period;
+
         end
         else begin
             signal<=0;
         end
-        new_count_quater_period=count_quater_period;
     end
 endmodule
